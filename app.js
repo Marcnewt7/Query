1aa8d5c880f7ad1c15058871abf62fae7b307a94
Marc newton <marc.newton07@gmail.com>
2024-03-06 04:23:16 -0700

    phew

---
+++ app.js
@@ -0,0 +1,8 @@
+$(".navItem").css("color", "white");
+$("#nav").css("background-color" , "#145");
+
+$("ul li:even").css("background-color" ,"#ccc");
+
+$("ul li:odd").css("background-color" ,"#aaa");
+
+$("input").val("Marc Newton");
